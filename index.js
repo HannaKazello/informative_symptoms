d53b8227972f323628cd7ef175eaf282f97e8ae0
HannaKazello <hanna.kazello@gmail.com>
2018-02-28 02:32:49 +0300

    finaly it works... maybe

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import XLSX from 'xlsx';
-import { normalizeMatrix } from './utils';
+import { normalizeMatrix, getCompactnessCoefficient, findRangeForSymptom } from './utils';
 
 const workbook = XLSX.readFile('sheet.xlsx');
 const sheetNameList = workbook.SheetNames;
@@ -10,4 +10,23 @@ const symptoms = Object.keys(healthy[0]).slice(1);
 const healthyTab = healthy.map(row => Object.values(row).slice(1));
 const sickTab = sick.map(row => Object.values(row).slice(1));
 
-console.log(normalizeMatrix([[1,2,3], [4,5,6], [7,8,9]]));
+const range = findRangeForSymptom(sickTab, healthyTab);
+
+const coeff = getCompactnessCoefficient(normalizeMatrix(healthyTab, range), normalizeMatrix(sickTab, range));
+const selection = symptoms.map((el, i) => {
+  const temp = {};
+  temp[el] = coeff[i];
+  return temp;
+});
+
+console.log('coeff: ', Array.from(selection).sort((a, b) => Object.values(a) - Object.values(b)));
+
+const range2 = findRangeForSymptom(normalizeMatrix(healthyTab, range), normalizeMatrix(sickTab, range));
+const area = [];
+selection.forEach((v, i) => {
+  if (range2.max[i] - range2.min[i] > Object.values(v)[0] * 2) {
+    area.push(Object.keys(v));
+  }
+});
+
+console.log('area: ', area);
--- utils.js
+++ utils.js
@@ -12,13 +12,14 @@ export function max(arr) {
 
 const normalizeValue = (value, minI, maxI) => (value - minI) / (maxI - minI);
 
-export function findRangeForSymptom(matrix) {
+export function findRangeForSymptom(matrix, m2) {
   const maxRange = [];
   const minRange = [];
   const transporedMatrix = transpose(matrix);
-  transporedMatrix.forEach((row) => {
-    maxRange.push(max(row));
-    minRange.push(min(row));
+  const transporedM2 = transpose(m2);
+  transporedMatrix.forEach((row, i) => {
+    maxRange.push(max(row.concat(transporedM2[i])));
+    minRange.push(min(row.concat(transporedM2[i])));
   });
   return {
     max: maxRange,
@@ -26,9 +27,20 @@ export function findRangeForSymptom(matrix) {
   };
 }
 
-export function normalizeMatrix(matrix) {
-  const range = findRangeForSymptom(matrix);
+export function normalizeMatrix(matrix, range) {
   return matrix.map(row =>
     row.map((el, i) => normalizeValue(el, range.min[i], range.max[i])),
   );
+}
+
+const calculateCoeff = (row1, row2) => {
+  return max(row1) - min(row2);
+};
+
+export function getCompactnessCoefficient(healthy, sick) {
+  const coeff = [];
+  const transporedSick = transpose(sick);
+  const transporedHealthy = transpose(healthy);
+  transporedHealthy.forEach((row, i) => coeff.push(calculateCoeff(row, transporedSick[i])));
+  return coeff;
 }
